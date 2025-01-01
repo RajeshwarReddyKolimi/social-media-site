a026f94d092d1c7ea48d05335be9d7dd890074c2
Rajeshwar <rajeshwar.kolimi@beautifulcode.in>
2025-01-01 16:58:07 +0530

    Added savedPosts buttons in profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { useRecoilState } from "recoil";
 import loadingState from "../../atoms/loadingState";
 import userState from "../../atoms/userState";
 import useAuth from "../../hooks/useAuth";
 import "./profile.css";
+import MyPosts from "./MyPosts";
+import SavedPosts from "./SavedPosts";
+import LikedPosts from "./LikedPosts";
 
 export default function Profile() {
   const [user, setUser] = useRecoilState(userState);
   const [loading, setLoading] = useRecoilState(loadingState);
   const { logout, getCurrentUser, signup, signin } = useAuth();
+  const [showItem, setShowItem] = useState("myPosts");
   const navigate = useNavigate();
   useEffect(() => {
     if (!loading && !user) {
@@ -23,6 +27,17 @@ export default function Profile() {
         <h1>{user?.name}</h1>
       </div>
       <button onClick={logout}>Logout</button>
+
+      <button onClick={() => setShowItem("myPosts")}>My Posts</button>
+      <button onClick={() => setShowItem("savedPosts")}>Saved Posts</button>
+      <button onClick={() => setShowItem("likedPosts")}>Liked Posts</button>
+      {showItem === "myPosts" ? (
+        <MyPosts />
+      ) : showItem === "savedPosts" ? (
+        <SavedPosts />
+      ) : (
+        showItem === "likedPosts" && <LikedPosts />
+      )}
     </div>
   );
 }
