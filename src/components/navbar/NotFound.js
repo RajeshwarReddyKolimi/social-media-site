2e9f400c20eca3efcbbd3fc1fa10c3b03926438c
Rajeshwar <rajeshwar.kolimi@beautifulcode.in>
2025-01-09 11:02:55 +0530

    NotFound page styling done:

--- src/components/navbar/NotFound.js
+++ src/components/navbar/NotFound.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "react-router";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>The page you are looking for does not exist.</h2>
+      <Link to="/">Go back to home</Link>
+    </div>
+  );
 }
